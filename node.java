d063d80ee4d3286b74254a787712970f16d22c0a
Adam Hudson <adamhudson7al@gmail.com>
2017-11-19 11:17:48 -0600

    init some files

---
+++ node.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Node{
+
+    //
+    int nodeID;
+    int routingTable[][];
+
+    // Constructor
+    public Node(int nodeID, int cost){
+        //
+        this.nodeID = nodeID;
+    }
+}
